8d3481b5cf08f75df218060974699ad6e98fc097
jday38 <jaaday@gmail.com>
2023-08-23 10:21:44 -0500

    1021 update

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-'''Get person's first name'''
-name = input("Your name: ")
-print(f"Welcome to beginning programing, {name}")
+''' This program determins how many slices of pizza each person in a group may eat
+@author James Day'''
+
+#Get number of slices and people
+numSlices = int(input("How many slices of pizza? "))
+numPeople = int(input("How many people are eating? "))
+
+#Deternube the number of slices for each person
+slicesPerPerson = numSlices / numPeople
+
+#Display the number of slices per person
+print()
+print(f"Number of slices per person {slicesPerPerson}")
